5d77ae9aee8439ffb20b88593bf01e16eb8d597d
vladian95 <kvankvan1995@gmail.com>
2022-04-15 15:40:52 +0300

    fixed plus

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,20 @@
-"use strict"
+//Accordion js
+const ACCORDION = document.getElementsByClassName("specific-accordion__item-wrapper");
+
+for (let i = 0; i < ACCORDION.length; i++) {
+    ACCORDION[i].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+
 
