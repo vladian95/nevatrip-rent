b7a1ae27fd61c716a5c588f10698d1eda74d2545
Ches <chesy47@gmail.com>
2022-04-15 18:29:35 +0300

    Tabs and accardion test JS

--- app/js/main.js
+++ app/js/main.js
@@ -1,39 +1,66 @@
 //Accordion JS
-const ACCORDION = document.getElementsByClassName("specific-accordion__item-wrapper");
+// const ACCORDION = document.getElementsByClassName("specific-accordion__item-wrapper");
 
-for (let i = 0; i < ACCORDION.length; i++) {
-  ACCORDION[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+// for (let i = 0; i < ACCORDION.length; i++) {
+//   ACCORDION[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
 
+//     this.nextElementSibling.style.display = this.nextElementSibling.style.display === 'none' ? 'block' : 'none' ; //ES6
+
+
+//     // const PANEL = this.nextElementSibling; 
+//     // if (PANEL.style.display === "block") {
+//     //   PANEL.style.display = "none";
+//     // } else {
+//     //   PANEL.style.display = "block"; //ES5
+//     // }
+
+
+//   });
+// }
 
 // Tabs 
-const tabs = document.querySelector(".tabs");
-const tabsBtns = tabs.querySelectorAll(".tabs__btn");
-const tabsContents = tabs.querySelectorAll(".tabs__content");
+// const tabs = document.querySelector(".tabs");
+// const tabsBtns = tabs.querySelectorAll(".tabs__btn");
+// const tabsContents = tabs.querySelectorAll(".tabs__content");
 
-function displayCurrentTab(current) {
-  for (let i = 0; i < tabsContents.length; i++) {
-    tabsContents[i].style.display = (current === i) ? "block" : "none";
-  }
-}
-displayCurrentTab(0);
+// function displayCurrentTab(current) {
+//   for (let i = 0; i < tabsContents.length; i++) {
+//     tabsContents[i].style.display = (current === i) ? "block" : "none";
+//   }
+// }
+// displayCurrentTab(0);
 
-tabs.addEventListener("click", event => {
-  if (event.target.className === "tabs__btn") {
-    for (let i = 0; i < tabsBtns.length; i++) {
-      if (event.target === tabsBtns[i]) {
-        displayCurrentTab(i);
-        break;
-      }
-    }
-  }
-});
+// tabs.addEventListener("click", event => {
+//   if (event.target.className === "tabs__btn") {
+//     for (let i = 0; i < tabsBtns.length; i++) {
+//       if (event.target === tabsBtns[i]) {
+//         displayCurrentTab(i);
+//         break;
+//       }
+//     }
+//   }
+// });
 
+
+//Sliders
+// let swiper = new Swiper(".mySwiper", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//     rotate: 50,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
