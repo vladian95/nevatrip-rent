149dd63a27215ed5bca82dd59c5b9bd7d420241b
Ches <chesy47@gmail.com>
2022-04-16 14:02:10 +0300

    Ship.page --imgs gradient

--- app/js/main.js
+++ app/js/main.js
@@ -1,55 +1,26 @@
 //Accordion JS
-// const ACCORDION = document.getElementsByClassName("specific-accordion__item-wrapper");
+const ACCORDION = document.getElementsByClassName("specific-accordion__item-wrapper");
 
-// for (let i = 0; i < ACCORDION.length; i++) {
-//   ACCORDION[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     let panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
+for (let i = 0; i < ACCORDION.length; i++) {
+  ACCORDION[i].addEventListener("click", function() {
+    this.classList.toggle("active");
 
-//     this.nextElementSibling.style.display = this.nextElementSibling.style.display === 'none' ? 'block' : 'none' ; //ES6
-
+    this.nextElementSibling.style.display = this.nextElementSibling.style.display === 'none' ? 'block' : 'none' ; //ES6
+  });
+}
 
 //     // const PANEL = this.nextElementSibling; 
 //     // if (PANEL.style.display === "block") {
 //     //   PANEL.style.display = "none";
 //     // } else {
-//     //   PANEL.style.display = "block"; //ES5
-//     // }
+//     //   PANEL.style.display = "block"; 
+//     // }   //ES5
 
 
 //   });
 // }
 
 // Tabs 
-// const tabs = document.querySelector(".tabs");
-// const tabsBtns = tabs.querySelectorAll(".tabs__btn");
-// const tabsContents = tabs.querySelectorAll(".tabs__content");
-
-// function displayCurrentTab(current) {
-//   for (let i = 0; i < tabsContents.length; i++) {
-//     tabsContents[i].style.display = (current === i) ? "block" : "none";
-//   }
-// }
-// displayCurrentTab(0);
-
-// tabs.addEventListener("click", event => {
-//   if (event.target.className === "tabs__btn") {
-//     for (let i = 0; i < tabsBtns.length; i++) {
-//       if (event.target === tabsBtns[i]) {
-//         displayCurrentTab(i);
-//         break;
-//       }
-//     }
-//   }
-// });
-
 const tabs = document.querySelector(".specific-session__tabs");
 const tabsBtns = tabs.querySelectorAll(".specific-session__btn-item");
 const tabsContents = tabs.querySelectorAll(".specific-session__content");
