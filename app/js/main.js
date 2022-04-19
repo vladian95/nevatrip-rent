c66ace5d1a64fd1fc51be9d5a995e5b4697a8370
Ches <chesy47@gmail.com>
2022-04-19 09:23:14 +0300

    Tabs And acc Css_2

--- app/js/main.js
+++ app/js/main.js
@@ -17,24 +17,39 @@
 
   
 
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-
-
-
-
-const ACCORDION = document.getElementsByClassName("specific-accordion__item-wrapper");
-
-for (let i = 0; i < ACCORDION.length; i++) {
-  ACCORDION[i].addEventListener("click", function() {
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
-    }
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
   });
 }
+
+
+
+
+
+
+// const ACCORDION = document.getElementsByClassName("specific-accordion__item-wrapper");
+
+// for (let i = 0; i < ACCORDION.length; i++) {
+//   ACCORDION[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     let panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
 
     // this.nextElementSibling.style.display = this.nextElementSibling.style.display === 'none' ? 'block' : 'none' ; //ES6
 
