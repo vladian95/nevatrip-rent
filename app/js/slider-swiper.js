5d8711d2e4aa1e34ffb0e66af47881e10601da85
Ches <chesy47@gmail.com>
2022-04-19 20:21:36 +0300

    CHes files

--- app/js/slider-swiper.js
+++ app/js/slider-swiper.js
@@ -2,6 +2,7 @@ const swiper = new Swiper('.swiper', {
 // loop: true,
   autoHeight: true,
   autoWidth: true,
+ 
   pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -13,26 +14,27 @@ const swiper = new Swiper('.swiper', {
     breakpoints: {
         360: {
             slidesPerView: 1,
-            // spaceBetween: 20,
+            spaceBetween: 20,
         },
+        // 460: {
+        //   slidesPerView: 2,
+        //   spaceBetween: 20,
+        // },
         768: {
             slidesPerView: 2,
             spaceBetween: 30,
         },
-        992: {
+        1200: {
           slidesPerView: 3,
-          spaceBetween: 30,
+          spaceBetween: 40,
           navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
             clickable: true,
           },
         },
-        1200: {
-          
-        },
         1440: {
-          
+          spaceBetween: 30,
       },
     },
   });
