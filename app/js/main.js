ad8b5d2e22cd8ddfdbcf4f79df52ea4c4d02c79e
vladian95 <kvankvan1995@gmail.com>
2022-05-04 23:19:10 +0300

    start popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,26 @@
 //Accordion JS
-const accordions = document.querySelectorAll('.accordion__item-wrapper');
+// const accordions = document.querySelectorAll('.accordion__item-wrapper');
 
-for(item of accordions) {
-    item.addEventListener('click', function() {
-        this.classList.toggle('accordion__item--active');
-    });
-}
+// for(item of accordions) {
+//     item.addEventListener('click', function() {
+//         this.classList.toggle('accordion__item--active');
+//     });
+// }
 
-$(document).ready(function() {
-    $('.accordion__item-wrapper').click(function() {
-        $(this).next('.accordion__dropDown').slideToggle();
-    });
+// $(document).ready(function() {
+//     $('.accordion__item-wrapper').click(function() {
+//         $(this).next('.accordion__dropDown').slideToggle();
+//     });
+// });
+
+
+
+
+$(".specific-transport__component").on('click', function() {
+    $(".custom-model-main").addClass('model-open');
+  }); 
+  $(".close-btn, .bg-overlay").click(function(){
+    $(".custom-model-main").removeClass('model-open');
 });
 
 
